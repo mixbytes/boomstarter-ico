cf4e064fd0ad367f62c0bca029c9200e1808e27d
Evgeny Pashentsev <ugnpaul@gmail.com>
2018-07-18 14:41:46 +0300

    Add an option to withdraw ether used for oraclize payments

--- contracts/BoomstarterICO.sol
+++ contracts/BoomstarterICO.sol
@@ -350,6 +350,16 @@ contract BoomstarterICO is ArgumentsChecker, ReentrancyGuard, EthPriceDependentF
         m_token.transfer(newICO, m_token.balanceOf(this));
     }
 
+    /// @notice withdraw all ether for oraclize payments
+    /// @param to Address to send ether to
+    function withdrawEther(address to)
+        public
+        validAddress(to)
+        onlymanyowners(keccak256(msg.data))
+    {
+        to.transfer(this.balance);
+    }
+
 
     // INTERNAL functions
 
