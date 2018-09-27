5da703c78fb7c34c394cc1669f7d853925551549
Eenae <z-line@my.com>
2018-09-27 14:27:22 +0300

    flattening contracts

---
+++ flatten.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Flattens contracts for etherscan verification
+#
+
+set -eu
+set -o pipefail
+
+TARGET_SUBDIR='build/flattened'
+
+BIN_DIR="$(cd $(dirname $0) && pwd)"
+cd "$BIN_DIR"
+
+
+rm -rf "$TARGET_SUBDIR"
+mkdir -p "$TARGET_SUBDIR"
+
+for F in contracts/*.sol;
+do
+    ./node_modules/.bin/truffle-flattener "$F" > "$TARGET_SUBDIR/$(basename "$F")"
+done
